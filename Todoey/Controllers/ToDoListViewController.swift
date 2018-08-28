9890c56500791d03483b1310a6bd8b8ebdea268c
Mike Gilhooly <mgilhooly@athleticloaner02>
2018-08-28 15:14:32 -0700

    broken help

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -12,11 +12,11 @@ import CoreData
 class ToDoListViewController: UITableViewController {
     
     
-    
-    var itemArray = [Item]()
+var itemArray = [Item]()
     let defaults = UserDefaults.standard
+        override func viewDidLoad() {
     
-    super.viewDidLoad() {
+            super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
     //if let items = defaults.array(forKey: "toDoListArray") as? [String] {
@@ -60,16 +60,9 @@ class ToDoListViewController: UITableViewController {
     
     
     
-    @IBOutlet weak var addBtnPressed: UIBarButtonItem!
-    override func viewDidLoad() {
-        
-        var textfield = UITextField()
-        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            
-            
+   
         }
-    }
+
         
         
         
@@ -104,7 +97,9 @@ class ToDoListViewController: UITableViewController {
     
     //MARK- Add Items
     
-        var textfield = UITextField()
+    @IBAction func addbuttonpressed(_ sender: Any) {
+    
+    var textfield = UITextField()
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
            
@@ -114,8 +109,8 @@ class ToDoListViewController: UITableViewController {
             self.defaults.set(self.itemArray, forKey: "toDoListArray")
             self.tableView.reloadData()
             
-            
-}
+        }
+
         
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = ("Create New Item")
@@ -125,7 +120,7 @@ class ToDoListViewController: UITableViewController {
         
         alert.addAction(action)
         
-        present (alert, animated: true, completion: nil)
+present(alert, animated: true, completion: nil)
         
     }
     
